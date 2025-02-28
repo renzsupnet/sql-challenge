bc1e1eb2aea3e0cff92b45362517282fac57a684
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2025-02-27 22:05:16 -0800

    Fixed multiple bugs mainly order of copy commands to avoid foreign key constraint, updated ERD

--- queries.sql
+++ queries.sql
@@ -1,76 +1,46 @@
--- Import data from departments.csv into Departments table
-COPY Departments(dep_no, dept_name)
-FROM '../data/departments.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
--- Import data from dept_emp.csv into DepartmentEmployee table
-COPY DepartmentEmployee(emp_no, dept_no)
-FROM '../data/dept_emp.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
--- Import data from dept_manager.csv into DepartmentManager table
-COPY DepartmentManager(dept_no, emp_no)
-FROM '../data/dept_manager.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
--- Import data from employees.csv into Employees table
-COPY Employees(emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date)
-FROM '../data/employees.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
--- Import data from salaries.csv into Salaries table
-COPY Salaries(emp_no, salary)
-FROM '../data/salaries.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
--- Import data from titles.csv into Titles table
-COPY Titles(title_id, title)
-FROM '../data/titles.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',');
-
 -- List the employee number, last name, first name, sex, and salary of each employee
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM Employees e
-JOIN Salaries s ON e.emp_no = s.emp_no;
+JOIN Salaries s ON e.emp_no = s.emp_no LIMIT 5;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986
 SELECT first_name, last_name, hire_date
 FROM Employees
-WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+WHERE EXTRACT(YEAR FROM hire_date) = 1986 LIMIT 5;
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM DepartmentManager dm
-JOIN Departments d ON dm.dept_no = d.dep_no
-JOIN Employees e ON dm.emp_no = e.emp_no;
+JOIN Departments d ON dm.dept_no = d.dept_no
+JOIN Employees e ON dm.emp_no = e.emp_no LIMIT 5;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name
-SELECT de.dep_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM DepartmentEmployee de
 JOIN Employees e ON de.emp_no = e.emp_no
-JOIN Departments d ON de.dep_no = d.dep_no;
+JOIN Departments d ON de.dept_no = d.dept_no LIMIT 5;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
 SELECT first_name, last_name, sex
 FROM Employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%' LIMIT 5;
 
 -- List each employee in the Sales department, including their employee number, last name, and first name
 SELECT e.emp_no, e.last_name, e.first_name
 FROM Employees e
 JOIN DepartmentEmployee de ON e.emp_no = de.emp_no
-JOIN Departments d ON de.dep_no = d.dep_no
-WHERE d.dept_name = 'Sales';
+JOIN Departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' LIMIT 5;
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM Employees e
 JOIN DepartmentEmployee de ON e.emp_no = de.emp_no
-JOIN Departments d ON de.dep_no = d.dep_no
-WHERE d.dept_name IN ('Sales', 'Development');
+JOIN Departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development') LIMIT 5;
 
 -- List the frequency counts, in descending order, of all the employee last names
 SELECT last_name, COUNT(*) AS frequency
 FROM Employees
 GROUP BY last_name
-ORDER BY frequency DESC;
+ORDER BY frequency DESC LIMIT 5;
--- schema.sql
+++ schema.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS Departments;
-DROP TABLE IF EXISTS DepartmentEmployee;
-DROP TABLE IF EXISTS DepartmentManager;
-DROP TABLE IF EXISTS Employees;
-DROP TABLE IF EXISTS Salaries;
-DROP TABLE IF EXISTS Titles;
+DROP TABLE IF EXISTS Departments CASCADE;
+DROP TABLE IF EXISTS DepartmentEmployee CASCADE;
+DROP TABLE IF EXISTS DepartmentManager CASCADE;
+DROP TABLE IF EXISTS Employees CASCADE;
+DROP TABLE IF EXISTS Salaries CASCADE; 
+DROP TABLE IF EXISTS Titles CASCADE;
 
 
 CREATE TABLE Departments(
-    dep_no VARCHAR(4) PRIMARY KEY,
+    dept_no VARCHAR(4) PRIMARY KEY,
     dept_name VARCHAR(25) NOT NULL
-)
+);
 
 CREATE TABLE Titles(
     title_id VARCHAR(10) PRIMARY KEY,
     title VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE Employees(
     emp_no INTEGER PRIMARY KEY,
@@ -26,32 +26,49 @@ CREATE TABLE Employees(
     hire_date DATE,
     FOREIGN KEY (emp_title_id)
     REFERENCES Titles(title_id)
-)
+);
 
 CREATE TABLE DepartmentEmployee(
-    dep_no VARCHAR(4),
+    dept_no VARCHAR(4),
     emp_no INTEGER,
-    PRIMARY KEY(dep_no, emp_no),
-    FOREIGN KEY(dep_no)
-    REFERENCES Departments(dep_no),
+    PRIMARY KEY(dept_no, emp_no),
+    FOREIGN KEY(dept_no)
+    REFERENCES Departments(dept_no),
     FOREIGN KEY(emp_no)
     REFERENCES Employees(emp_no)
-)
+);
 
 CREATE TABLE DepartmentManager(
     dept_no VARCHAR(4),
     emp_no INTEGER,
-    PRIMARY KEY(dep_no, emp_no),
-    FOREIGN KEY(dep_no)
-    REFERENCES Departments(dep_no),
+    PRIMARY KEY(dept_no, emp_no),
+    FOREIGN KEY(dept_no)
+    REFERENCES Departments(dept_no),
     FOREIGN KEY(emp_no)
     REFERENCES Employees(emp_no)
-)
+);
 
 CREATE TABLE Salaries(
     emp_no INTEGER PRIMARY KEY,
     salary DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY(emp_no)
     REFERENCES Employees(emp_no)
-)
+);
 
+-- Import data from departments.csv into Departments table
+\COPY Departments(dept_no, dept_name) FROM 'data/departments.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+-- Import data from titles.csv into Titles table
+\COPY Titles(title_id, title) FROM 'data/titles.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+-- Import data from employees.csv into Employees table
+\COPY Employees(emp_no, emp_title_id, birth_date, first_name, last_name, sex, hire_date) FROM './data/employees.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+-- Import data from salaries.csv into Salaries table
+\COPY Salaries(emp_no, salary) FROM 'data/salaries.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+-- Import data from dept_emp.csv into DepartmentEmployee table
+\COPY DepartmentEmployee(emp_no, dept_no) FROM 'data/dept_emp.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
+
+-- Import data from dept_manager.csv into DepartmentManager table
+\COPY DepartmentManager(dept_no, emp_no) FROM 'data/dept_manager.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
